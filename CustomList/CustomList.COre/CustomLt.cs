f9ddb72856b85c1a18577661163d5bedbccf66b1
Axmmehdi <ahmadsm.code.edu.az>
2023-05-02 03:24:16 +0400

    Upload

---
+++ CustomList/CustomList.COre/CustomLt.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CustomList.COre
+//{
+//    class CustomList<T>
+//    {
+//        private List<T> items;
+
+//        public CustomList()
+//        {
+//            items = new List<T>();
+//        }
+
+//        public void Add(T item)
+//        {
+//            items.Add(item);
+//        }
+
+//        public T Find(Predicate<T> match)
+//        {
+//            return items.Find(match);
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            CustomList<int> list = new CustomList<int>();
+//            list.Add(1);
+//            list.Add(2);
+//            list.Add(3);
+
+//            int result = list.Find(x => x == 2);
+
+//            Console.WriteLine(result); // Output: 2
+//            Console.WriteLine(list.Find(x => x == 4)); // Output: null
+//        }
+//    }
+
+//}
---
+++ CustomList/CustomList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CustomList<T>
+{
+    private List<T> items;
+
+    public CustomList()
+    {
+        items = new List<T>();
+    }
+
+    public void Add(T item)
+    {
+        items.Add(item);
+    }
+
+    public T Find(Predicate<T> match)
+    {
+        return items.Find(match);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CustomList<int> list = new CustomList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        int result = list.Find(x => x == 2);
+
+        Console.WriteLine(result);
+
+    }
+}
+
+
+
+
+
